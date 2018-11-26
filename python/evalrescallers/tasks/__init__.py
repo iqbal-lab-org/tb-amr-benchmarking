27f532670f2d0863ba7407b2d81f94c7185a868b
Martin Hunt <martin.g.hunt@gmail.com>
2018-11-26 12:28:45 +0000

    Remove tasks make_res_susc_bar_charts,summary_json_to_stats

--- python/evalrescallers/tasks/__init__.py
+++ python/evalrescallers/tasks/__init__.py
@@ -2,9 +2,7 @@ __all__ = [
     'make_summary_json',
     'run_callers_on_one_sample',
     'setup_pipeline_outdir',
-    'summary_json_to_stats',
     'download_nejm_reads',
-    'make_res_susc_bar_charts',
     'version',
 ]
 
