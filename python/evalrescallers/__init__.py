8d9e109cc06b2268e8ab0dbaaa6f93ce4cc04a5a
Martin Hunt <martin.g.hunt@gmail.com>
2018-11-26 12:33:30 +0000

    remove modules stats,performance_measurer

--- python/evalrescallers/__init__.py
+++ python/evalrescallers/__init__.py
@@ -1,11 +1,9 @@
 __all__ = [
     'ten_k_validation_data',
     'mykrobe_pub_data',
-    'performance_measurer',
     'pipeline_output_dir',
     'res_caller',
     'run_res_callers',
-    'stats',
     'tasks',
     'ten_k_reads_download',
     'utils',
